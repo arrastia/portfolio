cdde27878de763869a90e9993f83b621eec29c53
Arrastia <arrastia@pm.me>
2021-03-23 20:44:07 +0100

    add React project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from 'assets/img/logo.svg';
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className="App-link" href="https://instagram.com/pablo.arrastia" target="_blank" rel="noopener noreferrer">
+        Pablo Arrastia
+      </a>
+    </header>
+  </div>
+);
